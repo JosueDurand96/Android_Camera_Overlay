6c4952b4abd5560a81d082c1ff492f68c06c08ca
JosueDurand96 <josue.durand8@gmail.com>
2019-03-02 05:10:31 -0500

    efectos

--- app/src/main/java/mvvm/com/myapplication/MainActivity.java
+++ app/src/main/java/mvvm/com/myapplication/MainActivity.java
@@ -6,16 +6,20 @@ package mvvm.com.myapplication;
 import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Binder;
 import android.support.design.widget.CoordinatorLayout;
 
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,6 +34,9 @@ import com.zomato.photofilters.imageprocessors.subfilters.BrightnessSubFilter;
 import com.zomato.photofilters.imageprocessors.subfilters.ContrastSubFilter;
 import com.zomato.photofilters.imageprocessors.subfilters.SaturationSubfilter;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.util.List;
 
 
@@ -41,7 +48,7 @@ import mvvm.com.myapplication.Utils.BitmapUtils;
 
 
 public class MainActivity extends AppCompatActivity implements FiltersListFragmentListener,EditImageFragmentListener {
-    public static final String pictureName= "flash.jpg";
+    public static final String pictureName= "josue.jpg";
     public static final int PERMISSION_PICK_IMAGE=1000;
 
     ImageView img_preview;
@@ -71,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements FiltersListFragme
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Instagram Filter");
+        getSupportActionBar().setTitle("JOSUE EFECTO");
 
         //view
         img_preview=(ImageView)findViewById(R.id.image_preview);
@@ -187,6 +194,59 @@ public class MainActivity extends AppCompatActivity implements FiltersListFragme
     }
 
     private void saveImageGallery() {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted())
+                        {
+                            try {
+                                final String path = BitmapUtils.insertImage(getContentResolver(),
+                                        finalBitmap,
+                                        System.currentTimeMillis()+"_profile.jpg"
+                                        ,null);
+                                if (!TextUtils.isEmpty(path))
+                                {
+                                    Snackbar snackbar = Snackbar.make(coordinatorLayout,
+                                            "Imagen guardado en la galeria",
+                                            Snackbar.LENGTH_LONG)
+                                            .setAction("ABRIR", new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View view) {
+                                                    openImage(path);
+                                                }
+                                            });
+                                    snackbar.show();
+                                }
+                                else {
+                                    Snackbar snackbar = Snackbar.make(coordinatorLayout,
+                                            "No se pudo guardar la imagen",
+                                            Snackbar.LENGTH_LONG);
+                                    snackbar.show();
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "No tiene permiso", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                });
+    }
+
+    private void openImage(String path) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse(path),"image/*");
+        startActivity(intent);
     }
 
     private void openImageFromGallery() {
@@ -209,7 +269,7 @@ public class MainActivity extends AppCompatActivity implements FiltersListFragme
 
                  @Override
                  public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
-
+                     token.continuePermissionRequest();
                  }
              });
     }
@@ -221,6 +281,18 @@ public class MainActivity extends AppCompatActivity implements FiltersListFragme
             Bitmap bitmap = BitmapUtils.getBitmapFromGallery(this,data.getData(),800,800);
 
             //clear bitmap memory
+            originalBitmap.recycle();
+            finalBitmap.recycle();
+            filteredBitmap.recycle();
+
+            originalBitmap=bitmap.copy(Bitmap.Config.ARGB_8888,true);
+            finalBitmap=originalBitmap.copy(Bitmap.Config.ARGB_8888,true);
+            filteredBitmap=originalBitmap.copy(Bitmap.Config.ARGB_8888,true);
+            img_preview.setImageBitmap(originalBitmap);
+            bitmap.recycle();
+
+            //Render selected img thumbnail
+            filtersListFragment.displayThumbnail(originalBitmap);
 
         }
     }
